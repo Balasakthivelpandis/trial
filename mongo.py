bec57f9e6e18fd0e2bfe6772f007ab7868c7e68b
Balasakthivelpandis <125182721+Balasakthivelpandis@users.noreply.github.com>
2023-07-30 11:34:11 +0530

    Update mongo.py
    create table and insert table

--- mongo.py
+++ mongo.py
@@ -3,3 +3,9 @@ import pymongo
 myclient = pymongo.MongoClient("mongodb+srv://balasakthivelpandi:bala123@cloudmongodb.bxyay8l.mongodb.net/")
 
 mydb = myclient["mydatabase"]
+
+mycol = mydb["customers"]
+
+mydict = { "name": "John", "address": "Highway 37" }
+
+mydict
